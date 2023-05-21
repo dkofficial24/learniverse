8583d330665b249168981122d673c293ba334763
Dinesh Kumar <dkofficial24@gmail.com>
2023-05-21 16:18:30 +0530

    draft lession preview screen

---
+++ lib/lesson/ui/lesson_preview.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill ;
+import 'package:learniverse/lesson/model/lesson.model.dart';
+import 'package:learniverse/lesson/ui/lesson_edit_screen.dart';
+
+class LessonPreview extends StatelessWidget {
+  final Lesson lesson;
+
+  LessonPreview({required this.lesson});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Lesson Preview')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                lesson.title,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 8),
+              Text(
+                lesson.description,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              SizedBox(height: 16),
+              Container(
+                child: quill.QuillEditor.basic(
+                  controller: quill.QuillController(
+                    document: quill.Document.fromJson(jsonDecode(lesson.content)),
+                    selection: TextSelection.collapsed(offset: 0),
+                  ),readOnly: true
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Duration: ${lesson.durationInMinutes} minutes',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Text(
+                'Video URL: ${lesson.videoUrl}',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Text(
+                'Image URL: ${lesson.imageUrl}',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Text(
+                'Quiz duration: ${lesson.quizDurationInMinutes} minutes',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                child: Text('Edit'),
+                onPressed: () {
+
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
